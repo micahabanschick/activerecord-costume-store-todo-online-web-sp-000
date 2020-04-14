b806cd2cbab7d5a838eb8c5e5494c25e43030eaf
Micah Banschick <micha.ban@gmail.com>
2020-04-14 17:39:23 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,14 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costume_stores do |col|
+      col.string :name 
+      col.string :location
+      col.integer :costume_inventory
+      col.integer :employee_count
+      col.boolean :in_business?
+      col.integer :opening_time
+      col.integer :closing_time
+    end
+  end 
+end 
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,16 @@
 # Create your haunted_houses migration here
+
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+  def change
+    create_table :haunted_houses do |col|
+      col.string :name 
+      col.string :location
+      col.string :theme
+      col.integer :price
+      col.boolean :family_friendly?
+      col.integer :opening_time
+      col.integer :closing_time
+      col.string :long_description
+    end
+  end 
+end 
